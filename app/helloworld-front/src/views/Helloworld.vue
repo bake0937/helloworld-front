5f83ac9a7647bdca469d13bcf6f47371efe4869c
ryuta <komori@c-2-d.com>
2019-04-22 20:55:22 +0900

    不要コメント削除

--- app/helloworld-front/src/views/Helloworld.vue
+++ app/helloworld-front/src/views/Helloworld.vue
@@ -5,13 +5,7 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-
 import { HelloRequest, HelloResponse } from "@/pb/helloworld_service_pb";
-
-//import { grpc } from "@improbable-eng/grpc-web";
-//import { grpc } from 'grpc-web-client';
-
-
 import { GreeterClient } from "@/pb/helloworld_service_pb_service";
 
 @Component({})
@@ -23,7 +17,7 @@ export default class HelloworldView extends Vue {
 
     req.setName("world");
 
-    const svc = new GreeterClient("http://127.0.0.1:8888");
+    const svc = new GreeterClient("http://localhost:8888");
 
     svc.sayHello(req, (err, res) => {
       if (err || res === null) {
